9bcb5427cb19b69c2d421d16ea2f0de333bce835
Ravil2 <rafffa141105@gmail.com>
2025-06-14 20:14:38 +0500

    commit-1.2

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,7 +1,131 @@
+import {
+  Calendar,
+  ChevronUp,
+  Home,
+  Inbox,
+  Search,
+  Settings,
+  User2,
+} from 'lucide-react'
 import React from 'react'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarSeparator,
+} from './ui/sidebar'
+import Link from 'next/link'
+import Image from 'next/image'
+import { DropdownMenu } from './ui/dropdown-menu'
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@radix-ui/react-dropdown-menu'
+
+const items = [
+  {
+    title: 'Home',
+    url: '/',
+    icon: Home,
+  },
+  {
+    title: 'Inbox',
+    url: '#',
+    icon: Inbox,
+  },
+  {
+    title: 'Calendar',
+    url: '#',
+    icon: Calendar,
+  },
+  {
+    title: 'Search',
+    url: '#',
+    icon: Search,
+  },
+  {
+    title: 'Settings',
+    url: '#',
+    icon: Settings,
+  },
+]
 
 export default function AppSidebar() {
   return (
-    <div>AppSidebar</div>
+    <Sidebar collapsible="icon">
+      <SidebarHeader className='py-3'>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton className="flex" asChild>
+              <Link href="/">
+                <Image
+                  className="rounded-full"
+                  src="https://github.com/shadcn.png"
+                  width={30}
+                  height={30}
+                  alt="profile"
+                />
+                <span>Ravil Dev</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarSeparator />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> Ravil Dev <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                align="end"
+                className="border border-gray-600 rounded-2xl px-3 py-3 "
+              >
+                <DropdownMenuItem className=" outline-none cursor-pointer hover:bg-amber-50/20 px-2 py-1 rounded-sm">
+                  Account
+                </DropdownMenuItem>
+                <DropdownMenuItem className=" outline-none cursor-pointer hover:bg-amber-50/20 px-2 py-1 rounded-sm">
+                  Settings
+                </DropdownMenuItem>
+                <DropdownMenuItem className=" outline-none cursor-pointer hover:bg-amber-50/20 px-2 py-1 rounded-sm">
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
   )
 }
